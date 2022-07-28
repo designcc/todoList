0915293b0029d4b4c4b691a3d979acce2da723ff
pengfeicai <2218781313@qq.com>
2022-07-28 14:12:32 +0800

    完成功能

---
+++ src/components/EditTodo.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" :value="todoTitle" @input="onInputChange" v-bind="$attrs">
+</template>
+
+<script setup>
+import { toRef } from 'vue';
+
+const props = defineProps({
+  todoTitle: {
+    type: String,
+    default: true
+  }
+})
+const emit = defineEmits(['update:todoTitle'])
+const onInputChange = (e) => {
+  emit('update:todoTitle', e.target.value)
+}
+</script>
+
+<style scoped>
+
+</style>
